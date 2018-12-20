64c1ac978e14140f91a98257e3efbe6bee753789
MComScience <m-com3@hotmail.com>
2018-12-20 20:15:11 +0700

    add google analytics script

--- common/themes/homer/views/layouts/_base.php
+++ common/themes/homer/views/layouts/_base.php
@@ -49,6 +49,15 @@ $this->registerMetaTag([
     <?php $this->beginBody() ?>
     <?= $content ?>
     <?php $this->endBody() ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105362419-5"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-105362419-5');
+    </script>
     <?= Html::endTag('body') ?>
     </html>
 <?php $this->endPage() ?>
