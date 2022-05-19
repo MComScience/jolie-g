1243bae6f9a91c38bfc1fac9dddc5b71e79d4bc8
admin <pun.bxxx@gmail.com>
2022-05-19 10:19:53 +0700

    bug fix

--- common/themes/homer/modules/yii2-user/models/User.php
+++ common/themes/homer/modules/yii2-user/models/User.php
@@ -8,8 +8,93 @@
 namespace homer\user\models;
 
 use dektrium\user\models\User as BaseUser;
+use dektrium\user\helpers\Password;
+use dektrium\user\models\Token;
+
 
 class User extends BaseUser
 {
+ /**
+     * Creates new user account. If Module::enableGeneratingPassword is set true, this method
+     * will generate password.
+     *
+     * @return bool
+     */
+    public function create()
+    {
+        if ($this->getIsNewRecord() == false) {
+            throw new \RuntimeException('Calling "' . __CLASS__ . '::' . __METHOD__ . '" on existing user');
+        }
+
+        $transaction = $this->getDb()->beginTransaction();
+
+        try {
+            $this->password = ($this->password == null && $this->module->enableGeneratingPassword) ? Password::generate(8) : $this->password;
+
+            $this->trigger(self::BEFORE_CREATE);
+
+            if (!$this->save()) {
+                $transaction->rollBack();
+                return false;
+            }
+
+            $this->confirm();
+
+            // $this->mailer->sendWelcomeMessage($this, null, true);
+            $this->trigger(self::AFTER_CREATE);
+
+            $transaction->commit();
+
+            return true;
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            \Yii::warning($e->getMessage());
+            throw $e;
+        }
+    }
+
+     /**
+     * This method is used to register new user account. If Module::enableConfirmation is set true, this method
+     * will generate new confirmation token and use mailer to send it to the user.
+     *
+     * @return bool
+     */
+    public function register()
+    {
+        if ($this->getIsNewRecord() == false) {
+            throw new \RuntimeException('Calling "' . __CLASS__ . '::' . __METHOD__ . '" on existing user');
+        }
+
+        $transaction = $this->getDb()->beginTransaction();
+
+        try {
+            $this->confirmed_at = $this->module->enableConfirmation ? null : time();
+            $this->password     = $this->module->enableGeneratingPassword ? Password::generate(8) : $this->password;
+
+            $this->trigger(self::BEFORE_REGISTER);
+
+            if (!$this->save()) {
+                $transaction->rollBack();
+                return false;
+            }
+
+            if ($this->module->enableConfirmation) {
+                /** @var Token $token */
+                $token = \Yii::createObject(['class' => Token::className(), 'type' => Token::TYPE_CONFIRMATION]);
+                $token->link('user', $this);
+            }
+
+            // $this->mailer->sendWelcomeMessage($this, isset($token) ? $token : null);
+            $this->trigger(self::AFTER_REGISTER);
+
+            $transaction->commit();
+
+            return true;
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            \Yii::warning($e->getMessage());
+            throw $e;
+        }
+    }
 
 }
